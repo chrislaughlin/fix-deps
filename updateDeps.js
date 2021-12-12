65c1d4e8330083451cacb2f208c66c3d9b78b92f
Chris Laughlin <bluntm@gmail.com>
2021-12-12 18:57:32 +0000

    fixing file path

--- updateDeps.js
+++ updateDeps.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 module.exports = (dryRun) => {
     const packageJson = JSON.parse(
-        fs.readFileSync(`${__dirname}/package.json`, 'utf8')
+        fs.readFileSync(`${process.cwd()}/package.json`, 'utf8')
     );
     
     const {
@@ -14,7 +14,7 @@ module.exports = (dryRun) => {
     
     const checkDeps = (deps, type) => {
         Object.entries(deps).forEach(([depName, depVersion]) => {
-            const installedVersion = require(`${__dirname}/node_modules/${depName}/package.json`).version; 
+            const installedVersion = require(`${process.cwd()}/node_modules/${depName}/package.json`).version; 
             if (dryRun) {
                 console.log(`${depName}`);
                 console.log(`InstalledVersion: ${installedVersion}`);
@@ -30,5 +30,5 @@ module.exports = (dryRun) => {
     checkDeps(devDependencies, 'devDependencies');
     
     //save package.json
-    fs.writeFileSync(`${__dirname}/package.json`, JSON.stringify(updatedPackageJson, null, 4));
+    fs.writeFileSync(`${process.cwd()}/package.json`, JSON.stringify(updatedPackageJson, null, 4));
 };
